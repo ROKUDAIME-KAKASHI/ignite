8eb7e7340fede088e0a791cb7be9fd7347c2d3e1
ooa-jain <ooa.connect@jainuniversity.ac.in>
2026-07-17 11:12:08 +0530

    Clean up test scripts

--- frontend/test3.js
+++
@@ -1,2 +0,0 @@
-require('dotenv').config();
-const { logAudit } = require('./.next/server/app/actions/auth.js') || {}; // Wait, this is compiled. Let's just run a script using ts-node or run it via Next.js
--- frontend/test_race.js
+++
@@ -1,7 +0,0 @@
-const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error("Audit log timeout")), 1500));
-Promise.race([
-    Promise.resolve("SQL Success"),
-    timeout
-]).then(console.log).catch(console.error);
-
-setTimeout(() => console.log("Done waiting"), 2000);
